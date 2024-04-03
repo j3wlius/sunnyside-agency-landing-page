d479636d323cf5fb82cd43b35d9ee2cd3ad37b43
julius-tamale <juliotamex@proton.me>
2024-04-04 02:44:05 +0300

    Complete sunnyside agency landing project

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Sunnyside Agency Landing Page
+
+This project replicates the Sunnyside Agency Landing Page challenge from Frontend Mentor. The goal was to create a visually appealing and responsive landing page using HTML, CSS, and JavaScript.
+
+## Features
+
+**Responsive Layout**: The design adapts seamlessly across desktop, tablet, and mobile screen sizes.
+
+**Navigation**: A clear and easy-to-use navigation menu including a hamburger menu for smaller screens.
+
+
+[Live Demo](https://sunny-side-lpage.netlify.app/)
--- resources/js/index.js
+++ resources/js/index.js
@@ -10,3 +10,19 @@ const hamburgerToggle = () => {
 hamburger.addEventListener('click', hamburgerToggle);
 
 navLinks.forEach((link) => link.addEventListener('click', hamburgerToggle));
+
+// back to top btn
+const backToTopBtn = document.querySelector('#backToTopButton');
+
+window.addEventListener('scroll', () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = 'block';
+  } else {
+    backToTopBtn.style.display = 'none';
+  }
+});
+
+backToTopBtn.addEventListener('click', () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
